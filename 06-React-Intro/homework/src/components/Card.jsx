f9f33ea78a2bdb7c85f708d277a1ed7185e62729
Luciano Carducci <carduccilucianof@gmail.com>
2022-05-17 10:11:11 -0300

    Homework 06-React-Intro finished

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -6,13 +6,9 @@ export default function Card(props) {
 
     <button onClick={props.onClose}> X </button>
 
-    <h2>
-      {props.name}
-    </h2>
-    <h3>
-      Min {props.min}
-      Max {props.max}
-      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="image" />
-    </h3>
+    <h2> {props.name} </h2>
+    <h3> Min {props.min} </h3>
+    <h3> Max {props.max} </h3>
+     <h3> <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="image" /> </h3>
   </div>)
 };
