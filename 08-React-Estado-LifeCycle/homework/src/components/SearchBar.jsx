fac1d8271406a1cede148d018270d4613093acb3
Luciano Carducci <carduccilucianof@gmail.com>
2022-05-19 21:48:58 -0300

    Homework 09-React-Routing finished

--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -8,6 +8,7 @@ export default function SearchBar({onSearch}) {
     <form onSubmit={(e) => {
       e.preventDefault();
       onSearch(city);
+      setCity("")
     }}>
       <input
         type="text"
